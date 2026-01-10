43a4e646a058464460decfd2027475bdb0c8c64e
Gabot3ck <jesus.castilloc@outlook.com>
2026-01-10 19:30:43 -0300

    se incia con las colecciones en /src/blog

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import { defineCollection, z } from "astro:content";
+
+
+
+const BlogCollections = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    description: z.string(),
+    image: z.string(),
+
+    // relación
+    author: z.string(),
+    
+    // relación
+    tags: z.array(z.string()),
+  })
+})
+
+export const collections = {
+  blog: BlogCollections,
+};
